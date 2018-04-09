221df881082a7fa00204c3ead8d76856f060e57b
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2018-04-09 09:24:15 -0700

    improved get_datasets so it can filter out attrs and types

--- get_datasets.py
+++ get_datasets.py
@@ -7,19 +7,24 @@ def convert_to_numpy_array(query_results):
   as_2d_array = list(map(lambda x: list(x), query_results))
   return np.array(as_2d_array)
 
-def get_data(types):
+def get_data(types, attrs):
   conn = sqlite3.connect("routes.db")
-  data = None
-  if types is None:
-      data = conn.cursor().execute("SELECT ROUND(stars), latitude, longitude, saftey, difficulty, Trad, Ice, Sport, TR, Alpine, Snow, Mixed, Aid, Boulder, Other FROM routes").fetchall()
-  else:
+  attr_clause = "latitude, longitude, saftey, difficulty, Trad, Ice, Sport, TR, Alpine, Snow, Mixed, Aid, Boulder, Other"
+  where_clause = None
+  if types is not None:
       types = [ " %s = 1" % t for t in types]
       where_clause = " OR ".join(types)
-      data = conn.cursor().execute("SELECT ROUND(stars), latitude, longitude, saftey, difficulty FROM routes WHERE %s" % where_clause).fetchall()
+      attr_clause = "latitude, longitude, saftey, difficulty"
+  if attrs is not None:
+      attr_clause = ", ".join(attrs)
+  query = ("SELECT ROUND(stars), %s FROM routes" % attr_clause)
+  if where_clause is not None:
+      query += " WHERE " + where_clause
+  data = conn.cursor().execute(query).fetchall()
   return convert_to_numpy_array(data)
 
-def get_datasets(types=None):
-   route_data = get_data(types)
+def get_datasets(types=None, attrs=None,):
+   route_data = get_data(types, attrs)
    stars = route_data[:, 0]
    data = route_data[:, 1:]
    return train_test_split(data, stars, test_size=0.20)
