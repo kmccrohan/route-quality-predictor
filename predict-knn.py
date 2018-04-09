c74b0ad33af6df671014431edbd3852dc9083e66
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2018-04-09 11:46:41 -0700

    knn k from command line

--- predict-knn.py
+++ predict-knn.py
@@ -1,15 +1,14 @@
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
 from get_datasets import get_datasets
-
-K = 15
+import sys
 
 def main():
 
    data_train, data_test, stars_train, stars_test = get_datasets(attrs=["latitude","longitude"])
    print("Training size: %d" % len(data_train))
 
-   knn=neighbors.KNeighborsClassifier(n_neighbors=K)
+   knn=neighbors.KNeighborsClassifier(n_neighbors=int(sys.argv[1]))
    knn.fit(data_train, stars_train)
    predictions = knn.predict(data_test)
    print("Accuracy: %f\n" % accuracy_score(stars_test, predictions))
