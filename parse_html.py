c16d2a29fb20a3f4210579129d430d3e080307a4
Ethan Mahintorabi <ethan.mahintorabi@ibm.com>
2018-04-18 20:00:53 -0700

    description parser.

---
+++ parse_html.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import sqlite3
+
+def main():
+
+  connection = sqlite3.connect("routes.db")
+  routes = connection.execute("SELECT html,url FROM routes")
+  for route in routes:
+    html = route[0]
+    soup = BeautifulSoup(html, "html.parser")
+    description = soup.select_one(".fr-view")
+    connection.execute("ALTER TABLE routes ADD COLUMN description_length INT")
+    print(route[1])
+    if description:
+      raw_text = description.get_text()
+      connection.execute("UPDATE routes SET description = ?, description_length = ? WHERE url = ?", (raw_text, len(raw_text), route[1]))
+    else:
+      connection.execute("UPDATE routes SET description = ?, description_length = ? WHERE url = ?", ("", 0,route[1]))
+    
+    connection.commit()
+
+main()
